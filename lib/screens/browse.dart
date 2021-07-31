559e999e47b49882c4594b6470af24dd35139c99
Rodrigo Tavares <rodrigost23@gmail.com>
2021-07-31 19:02:50 -0400

    Add mockup list of apps

--- lib/screens/browse.dart
+++ lib/screens/browse.dart
@@ -12,27 +12,83 @@ class _BrowsePageState extends State<BrowsePage> {
   var index = 0;
   @override
   Widget build(BuildContext context) {
+    //TODO: Remove sample:
+    const apps = {
+      "7-zip": {"version": "19.00", "bucket": "main"},
+      "python": {"version": "3.6.9", "bucket": "main"},
+    };
+
+    final _controller = ScrollController();
+
     return ScaffoldPage(
       header: PageHeader(
         title: Text("Browse apps"),
         commandBar: Button(child: Text("Refresh"), onPressed: () => {}),
       ),
-      content: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 16.0),
-            child: ToggleButtonBar(
-              selected: index,
-              onChanged: (i) => setState(() => index = i),
-              items: [
-                CustomToggleButton(child: Text('All')),
-                CustomToggleButton(child: Text('bucket-1')),
-                CustomToggleButton(child: Text('bucket-2')),
+      padding: EdgeInsets.only(top: 24),
+      content: Container(
+        decoration: BoxDecoration(
+          color: FluentTheme.of(context).activeColor,
+          border: Border.all(
+            color: FluentTheme.of(context).inactiveBackgroundColor,
+            width: 1,
+          ),
+          borderRadius: BorderRadius.circular(8.0),
+        ),
+        child: Scrollbar(
+          controller: _controller,
+          child: Padding(
+            padding:
+                const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                ToggleButtonBar(
+                  selected: index,
+                  onChanged: (i) => setState(() => index = i),
+                  items: [
+                    CustomToggleButton(child: Text('All')),
+                    CustomToggleButton(child: Text('bucket-1')),
+                    CustomToggleButton(child: Text('bucket-2')),
+                  ],
+                ),
+                Expanded(
+                  child: ListView.builder(
+                    itemCount: apps.length,
+                    itemBuilder: (context, index) {
+                      final title = apps.keys.toList()[index];
+                      final app = apps[title];
+                      return ListTile(
+                        title: Row(
+                          crossAxisAlignment: CrossAxisAlignment.end,
+                          children: [
+                            Text(title),
+                            Padding(
+                              padding: const EdgeInsets.only(left: 8.0),
+                              child: Text(
+                                app!["version"]!,
+                                style: FluentTheme.of(context)
+                                    .typography
+                                    .caption
+                                    ?.copyWith(
+                                      color:
+                                          FluentTheme.of(context).accentColor,
+                                    ),
+                              ),
+                            ),
+                          ],
+                        ),
+                        subtitle: Text((app["bucket"]!)),
+                        trailing:
+                            Button(child: Text("Install"), onPressed: () => {}),
+                      );
+                    },
+                  ),
+                )
               ],
             ),
           ),
-        ],
+        ),
       ),
     );
   }
